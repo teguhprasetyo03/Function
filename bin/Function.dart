fc1161c10a255f0e258222810bf1d4ce481343a1
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2021-03-12 20:43:19 +0700

    Initial commit

---
+++ bin/Function.dart
@@ -0,0 +1,16 @@
+void main(){
+  hello();
+  hitungluas();
+}
+
+hello(){
+    print("Selamat Datang Di Kelas Coding");
+    print("Kita Sedang Belajar Dart");
+}
+
+hitungluas(){
+  var panjang = 20;
+  var tinggi = 15;
+  var luas = panjang*tinggi;
+  print(luas);
+}
