6d62de702c48a9356802a41e2259b1f09d0135cf
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2021-03-19 21:26:52 +0700

    membuat tutorial function parameter

---
+++ bin/FunctionParameter.dart
@@ -0,0 +1,15 @@
+void main(){
+  SayHi('Andi', 20);
+  SayHi('Budi', 21);
+  SayHi('Jono', 22);
+  HitungLuas(20, 20);
+}
+
+void SayHi(String name, int age){
+    print('Hello My Name is $name, My age $age');
+}
+
+void HitungLuas(int panjang, int lebar){
+    int luas = panjang*lebar;
+    print('Luasnya adalah = $luas');
+}
